ec2a9d8e9e5eff5f55da24a67a09e7165090f8f8
Fikadu F.Shanko <1988.fikadufilipos@gmail.Com>
2021-09-06 08:07:45 -0500

    decorate my text lab added

---
+++ decorateMyText/decoratemytext.js
@@ -0,0 +1,9 @@
+
+function myfunction(){
+     document.getElementById("txtarea1").style.fontSize = "2pt larger";
+}
+function myfunction1(){
+    document.getElementById("txtarea1").style.fontWeight = "bold";
+    document.getElementById("txtarea1").style.color = "green";
+    document.getElementById("txtarea1").style.textDecoration = "underline";
+}
